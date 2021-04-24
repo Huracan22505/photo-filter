90600d5850dee9eae05377f21f91c10f3bf7c8a3
Alexander Huracan <Nokia2611@mail.ru>
2021-04-24 18:01:24 +0300

    feat: add reset btn logic

--- photo-filter/index.js
+++ photo-filter/index.js
@@ -1,19 +1,19 @@
 const inputs = document.querySelectorAll(".filters input");
 // const outputs = document.querySelectorAll("output");
-// const reset = document.querySelector(".btn-reset");
+const reset = document.querySelector(".btn-reset");
 const img = document.querySelector("img");
 
 inputs.forEach((input) => input.addEventListener("input", hendleUpdate));
-// reset.addEventListener("click", () => {
-//   inputs.forEach((input) => {
-//     input.value = input.defaultValue;
-//     const output = document.querySelector(
-//       `input[name="${input.name}"] ~ output`
-//     );
-//     output.textContent = input.value;
-//   });
-//   img.style = "";
-// });
+reset.addEventListener("click", () => {
+  inputs.forEach((input) => {
+    input.value = input.defaultValue;
+    const output = document.querySelector(
+      `input[name="${input.name}"] ~ output`
+    );
+    output.textContent = input.value;
+  });
+  img.style = "";
+});
 
 function hendleUpdate() {
   const suffix = this.dataset.sizing || "";
