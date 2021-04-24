0af351f67c6bfb0cddc9498820251d67756d183a
Alexander Huracan <Nokia2611@mail.ru>
2021-04-24 18:05:13 +0300

    refactor: make clean code

--- photo-filter/index.js
+++ photo-filter/index.js
@@ -1,10 +1,20 @@
 const inputs = document.querySelectorAll(".filters input");
 // const outputs = document.querySelectorAll("output");
-const reset = document.querySelector(".btn-reset");
+const resetBtn = document.querySelector(".btn-reset");
 const img = document.querySelector("img");
 
 inputs.forEach((input) => input.addEventListener("input", hendleUpdate));
-reset.addEventListener("click", () => {
+resetBtn.addEventListener("click", onResetBtnClick);
+
+function hendleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  img.style.setProperty(`--${this.name}`, this.value + suffix);
+
+  const output = document.querySelector(`input[name="${this.name}"] ~ output`);
+  output.textContent = this.value;
+}
+
+function onResetBtnClick() {
   inputs.forEach((input) => {
     input.value = input.defaultValue;
     const output = document.querySelector(
@@ -12,14 +22,6 @@ reset.addEventListener("click", () => {
     );
     output.textContent = input.value;
   });
+
   img.style = "";
-});
-
-function hendleUpdate() {
-  const suffix = this.dataset.sizing || "";
-  img.style.setProperty(`--${this.name}`, this.value + suffix);
-
-  const output = document.querySelector(`input[name="${this.name}"] ~ output`);
-
-  output.textContent = this.value;
 }
