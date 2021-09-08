7bb0203e1679f09174a918471cf26cd5451080d4
Huracan22505 <nokia2611@mail.ru>
2021-09-08 11:33:05 +0300

    feat: swap to TS

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import { appRender } from "./app";
-import "./style.css";
+import { appRender } from './app';
+import './style.css';
 
 appRender();
 
-const filters = document.querySelectorAll(".filters input");
-const outputs = document.querySelectorAll("output");
-const resetBtn = document.querySelector(".btn-reset");
-const nextBtn = document.querySelector(".btn-next");
-const img = document.querySelector("img");
-const fullscreenBtn = document.querySelector(".fullscreen");
+const filters = document.querySelectorAll<HTMLInputElement>('.filters input');
+const outputs = document.querySelectorAll('output');
+const resetBtn = document.querySelector('.btn-reset') as HTMLInputElement;
+const nextBtn = document.querySelector('.btn-next') as HTMLElement;
+const img = document.querySelector('img') as any;
+const fullscreenBtn = document.querySelector('.fullscreen') as HTMLElement;
+const loadLabel = document.querySelector(
+  'label[for="btnInput"]',
+) as HTMLElement;
+const loadInput = document.querySelector(
+  'input[type="file"]',
+) as HTMLInputElement;
+const saveBtn = document.querySelector('.btn-save') as HTMLElement;
 
 // fullscreen button logic
 
-fullscreenBtn.addEventListener("click", () => {
+fullscreenBtn.addEventListener('click', () => {
   const app = document.documentElement;
 
   app.requestFullscreen();
@@ -20,66 +27,75 @@ fullscreenBtn.addEventListener("click", () => {
 });
 
 // filters change logic
-
-filters.forEach((input) => input.addEventListener("input", onFilterChange));
-
-function onFilterChange() {
-  const suffix = this.dataset.sizing || "";
+function onFilterChange(this: HTMLInputElement) {
+  const suffix = this.dataset.sizing || '';
   img.style.setProperty(`--${this.name}`, this.value + suffix);
 
-  const output = document.querySelector(`input[name="${this.name}"] ~ output`);
+  const output = document.querySelector(
+    `input[name="${this.name}"] ~ output`,
+  ) as HTMLOutputElement;
   output.value = this.value;
 }
 
+filters.forEach(input => input.addEventListener('input', onFilterChange));
+
 // reset button logic
-
-resetBtn.addEventListener("click", onResetBtnClick);
-
-function onResetBtnClick() {
-  filters.forEach((filter) => {
+const onResetBtnClick = () => {
+  filters.forEach(filter => {
+    // eslint-disable-next-line no-param-reassign
     filter.value = filter.defaultValue;
+
     const output = document.querySelector(
-      `input[name="${filter.name}"] ~ output`
-    );
+      `input[name="${filter.name}"] ~ output`,
+    ) as HTMLOutputElement;
+
     output.value = filter.value;
   });
 
-  img.style = "";
+  img.setAttribute('style', '');
 
-  nextBtn.classList.remove("btn-active");
-  loadLabel.classList.remove("btn-active");
-  saveBtn.classList.remove("btn-active");
-  resetBtn.classList.add("btn-active");
-}
+  nextBtn.classList.remove('btn-active');
+  loadLabel.classList.remove('btn-active');
+  saveBtn.classList.remove('btn-active');
+  resetBtn.classList.add('btn-active');
+};
+
+resetBtn.addEventListener('click', onResetBtnClick);
 
 // next button logic
 
 const BASE_URL =
-  "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/";
+  'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
 let counter = 0;
 const images = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "05.jpg",
-  "06.jpg",
-  "07.jpg",
-  "08.jpg",
-  "09.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
-  "13.jpg",
-  "14.jpg",
-  "15.jpg",
-  "16.jpg",
-  "17.jpg",
-  "18.jpg",
-  "19.jpg",
-  "20.jpg",
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '05.jpg',
+  '06.jpg',
+  '07.jpg',
+  '08.jpg',
+  '09.jpg',
+  '10.jpg',
+  '11.jpg',
+  '12.jpg',
+  '13.jpg',
+  '14.jpg',
+  '15.jpg',
+  '16.jpg',
+  '17.jpg',
+  '18.jpg',
+  '19.jpg',
+  '20.jpg',
 ];
 
-nextBtn.addEventListener("click", onNextBtnClick);
+function createImage(src: string | ArrayBuffer | null) {
+  const image: any = new Image();
+  image.src = src;
+  image.onload = () => {
+    img.src = `${src}`;
+  };
+}
 
 function onNextBtnClick() {
   const index = counter % images.length;
@@ -88,90 +104,82 @@ function onNextBtnClick() {
   const hours = date.getHours();
   let timeOfDay;
 
-  if (hours >= 6 && hours <= 11) timeOfDay = "morning/";
-  if (hours >= 12 && hours <= 17) timeOfDay = "day/";
-  if (hours >= 18 && hours <= 23) timeOfDay = "evening/";
-  if (hours >= 0 && hours <= 5) timeOfDay = "night/";
+  if (hours >= 6 && hours <= 11) timeOfDay = 'morning/';
+  if (hours >= 12 && hours <= 17) timeOfDay = 'day/';
+  if (hours >= 18 && hours <= 23) timeOfDay = 'evening/';
+  if (hours >= 0 && hours <= 5) timeOfDay = 'night/';
 
   const imageSrc = `${BASE_URL}${timeOfDay}${images[index]}`;
 
   createImage(imageSrc);
-  counter++;
+  counter += 1;
 
-  resetBtn.classList.remove("btn-active");
-  loadLabel.classList.remove("btn-active");
-  saveBtn.classList.remove("btn-active");
-  nextBtn.classList.add("btn-active");
+  resetBtn.classList.remove('btn-active');
+  loadLabel.classList.remove('btn-active');
+  saveBtn.classList.remove('btn-active');
+  nextBtn.classList.add('btn-active');
 }
 
-function createImage(src) {
-  const image = new Image();
-  image.src = src;
-  image.onload = () => {
-    img.src = `${src}`;
-  };
-}
+nextBtn.addEventListener('click', onNextBtnClick);
 
 // load button logic
 
-const loadLabel = document.querySelector('label[for="btnInput"]');
-const loadInput = document.querySelector('input[type="file"]');
+loadInput.addEventListener('change', (event): void => {
+  if (!loadInput.files) return;
+  let { value } = <HTMLInputElement>event.currentTarget;
 
-loadInput.addEventListener("change", function (e) {
   const file = loadInput.files[loadInput.files.length - 1];
-  console.log(loadInput.files);
+
   const reader = new FileReader();
 
   reader.onload = () => {
     img.src = reader.result;
   };
+
   reader.readAsDataURL(file);
 
-  e.currentTarget.value = "";
+  value = '';
 
-  nextBtn.classList.remove("btn-active");
-  resetBtn.classList.remove("btn-active");
-  saveBtn.classList.remove("btn-active");
-  loadLabel.classList.add("btn-active");
+  nextBtn.classList.remove('btn-active');
+  resetBtn.classList.remove('btn-active');
+  saveBtn.classList.remove('btn-active');
+  loadLabel.classList.add('btn-active');
 });
 
 // canvas logic
 
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 
 function drawImage() {
   const image = new Image();
-  image.setAttribute("crossOrigin", "anonymous");
+  image.setAttribute('crossOrigin', 'anonymous');
   image.src = img.src;
 
-  image.onload = function () {
+  image.onload = () => {
     canvas.width = image.width;
     canvas.height = image.height;
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
 
-    ctx.filter = `blur(${outputs[0].value * 1.6}px) invert(${
+    ctx.filter = `blur(${Number(outputs[0].value) * 1.6}px) invert(${
       outputs[1].value
     }%) sepia(${outputs[2].value}%) saturate(${outputs[3].value}%) hue-rotate(${
       outputs[4].value
     }deg)`;
-    console.log("image.onload -> ctx.filter", ctx.filter);
     ctx.drawImage(image, 0, 0);
 
-    var link = document.createElement("a");
-    link.download = "download.png";
+    const link = document.createElement('a');
+    link.download = 'download.png';
     link.href = canvas.toDataURL();
     link.click();
-    link.delete;
   };
 
-  nextBtn.classList.remove("btn-active");
-  resetBtn.classList.remove("btn-active");
-  loadLabel.classList.remove("btn-active");
-  saveBtn.classList.add("btn-active");
+  nextBtn.classList.remove('btn-active');
+  resetBtn.classList.remove('btn-active');
+  loadLabel.classList.remove('btn-active');
+  saveBtn.classList.add('btn-active');
 }
 
 // save button logic
 
-const saveBtn = document.querySelector(".btn-save");
-
-saveBtn.addEventListener("click", drawImage);
+saveBtn.addEventListener('click', drawImage);
